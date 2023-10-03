7d33b82163215a73ec88947f611885be1c88ee72
Christopher Aulotte <caulotte@Aerial-Feather.local>
2023-10-03 23:00:03 +1100

    Fix: Usage of official copilot.

--- lua/devaulotte/plugins/copilot.lua
+++ lua/devaulotte/plugins/copilot.lua
@@ -1,21 +1,7 @@
 return {
   {
-    "zbirenbaum/copilot.lua",
+    "github/copilot.vim",
     cmd = "Copilot",
     event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestions = { enabled = false },
-        panel = { enabled = false },
-      })
-    end,
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    event = "InsertEnter",
-    dependencies = { "zbirenbaum/copilot.lua" },
-    config = function()
-      require("copilot_cmp").setup()
-    end,
   },
 }
