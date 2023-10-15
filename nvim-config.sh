9085506cd8e1ef95cc2c90351bc05d6808a08e37
Dev'A'Lot <dev.aulotte@gmail.com>
2023-10-16 10:04:14 +1100

    Feat: Add condition for update package
    Add condition to remove configuration and update neovim configuration

--- nvim-config.sh
+++ nvim-config.sh
@@ -2,12 +2,23 @@
 
 # Check if ther is a nvim directory in ~/.config
 if [ -d ~/.config/nvim ]; then # If there is no nvim directory, create one
-	mv ~/.config/nvim ~/.config/nvim.bak
-	mv ~/.local/share/nvim ~/.local/share/nvim.bak
-	echo "Old nvim config moved to ~/.config/nvim.bak"
+	read -p "There is already a nvim config in ~/.config/nvim. Do you want to update it? (y/n) " -n 1 -r
 	echo ""
+
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		echo "Nvim update..."
+		rm -rf ~/.config/nvim ~/.config/nvim.bak
+		echo ""
+		echo "Nvim config updated!"
+		echo ""
+	else
+		mv ~/.config/nvim ~/.config/nvim.bak
+		mv ~/.local/share/nvim ~/.local/share/nvim.bak
+		echo "Old nvim config moved to ~/.config/nvim.bak"
+		echo ""
+		cp -r nvim ~/.config/nvim
+		echo "Neovim config added to ~/.config/nvim"
+		echo ""
+	fi
 fi
 
-cp -r nvim ~/.config/nvim
-echo "Neovim config added to ~/.config/nvim"
-echo ""
