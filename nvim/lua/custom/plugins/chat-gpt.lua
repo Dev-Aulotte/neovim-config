9e6107e5157a44d779c092d2d3968bf57c6f41aa
Dev'A'Lot <dev.aulotte@gmail.com>
2023-10-23 09:28:02 +1100

    Fix: Remove openAI key

--- nvim/lua/custom/plugins/chat-gpt.lua
+++ nvim/lua/custom/plugins/chat-gpt.lua
@@ -4,7 +4,7 @@ return {
 		event = "VeryLazy",
 		config = function()
 			require("chatgpt").setup({
-				async_api_key_cmd = "echo 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",
+				async_api_key_cmd = "",
 			})
 		end,
 		dependencies = {
